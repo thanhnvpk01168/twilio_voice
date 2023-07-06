bd1046f934d78a8fece85a68e468ebb08603ed13
Van Thanh <thanhnguyen@gkim.vn>
2023-07-06 16:31:47 +0700

    update

--- ios/Classes/SwiftTwilioVoicePlugin.swift
+++ ios/Classes/SwiftTwilioVoicePlugin.swift
@@ -212,6 +212,10 @@ public class SwiftTwilioVoicePlugin: NSObject, FlutterPlugin,  FlutterStreamHand
             }
             
         }else if flutterCall.method == "hangUp"{
+            if let call = self.call {
+                self.sendPhoneCallEvents(description: "LOG|provider:performEndCallAction: disconnecting call", isError: false)
+                call.disconnect()
+            }
             if (self.call != nil && self.call?.state == .connected) {
                 self.sendPhoneCallEvents(description: "LOG|hangUp method invoked", isError: false)
                 self.userInitiatedDisconnect = true
